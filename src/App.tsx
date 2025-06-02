bd4caef5df68352a661cbc5273d5ff67fce30d87
Abhishek Mathur <132328327+AbhishekMathur25@users.noreply.github.com>
2025-06-02 12:27:45 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Hotels from './pages/Hotels';
+import TripPlanner from './pages/TripPlanner';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/hotels" element={<Hotels />} />
+              <Route path="/trip-planner" element={<TripPlanner />} />
+            </Routes>
+          </main>
+          <Toaster position="bottom-right" />
+        </div>
+      </Router>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
